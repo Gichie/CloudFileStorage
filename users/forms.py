cf240e35b4bd6378a0f8786dab024cc5527c93eb
gichie <Gichie@mail.ru>
2025-04-19 09:13:19 +0300

    create users.forms

---
+++ users/forms.py
@@ -0,0 +1,66 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm
+
+
+class LoginUserForm(AuthenticationForm):
+    username = forms.CharField(
+        label='Имя пользователя / E-mail',
+        widget=forms.TextInput(attrs={'class': 'form-input'})
+    )
+    password = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={'class': 'form-input'})
+    )
+
+
+User = get_user_model()
+
+
+class RegistrationForm(forms.ModelForm):
+    username = forms.CharField(
+        min_length=2,
+        label='Имя пользователя',
+        widget=forms.TextInput(attrs={'class': 'form-input'})
+    )
+    password = forms.CharField(
+        min_length=2,
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={'class': 'form-input'})
+    )
+    password2 = forms.CharField(
+        label='Подтвердите пароль',
+        widget=forms.PasswordInput(attrs={'class': 'form-input'})
+    )
+    email = forms.EmailField(
+        label='E-mail',
+        required=False,
+        widget=forms.EmailInput(attrs={'placeholder': 'Необязательно', 'class': 'form-input'})
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+    def clean_password(self):
+        form_data = self.cleaned_data
+        password = form_data.get('password')
+        password2 = form_data.get('password2')
+        if password and password2 and password != password2:
+            raise forms.ValidationError('Пароли не совпадают')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if email:
+            email = email.lower()
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError('Такой E-mail уже существует')
+        return email
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+
+        if commit:
+            user.save()
+        return user
