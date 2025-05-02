a0bb253b82ac615f2e7a237b57df1f34f7976292
gichie <Gichie@mail.ru>
2025-05-02 23:25:14 +0300

    update

--- file_storage/forms.py
+++ file_storage/forms.py
@@ -1,5 +1,19 @@
 from django import forms
 
+from file_storage.models import UserFile
 
-class FileUploadForm(forms.Form):
-    file = forms.FileField(label="Выберите файл для погрузки")
+
+class FileUploadForm(forms.ModelForm):
+    class Meta:
+        model = UserFile
+        fields = ['file']
+
+    def clean_file(self):
+        file = self.cleaned_data.get('file')
+        if file:
+            # Валидация на размер файла(не больше 100 МБ)
+            if file.size > 100 * 1024 * 1024:
+                raise forms.ValidationError("Файл слишком большой. Максимальный размер - 100 МБ.")
+            return file
+        else:
+            raise forms.ValidationError("Файл отсутствует или его не удалось прочитать.")
