22e1a9e74ed4af3af3ab0cb451379623eea6a32a
gichie <Gichie@mail.ru>
2025-05-02 23:26:05 +0300

    update

--- file_storage/urls.py
+++ file_storage/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from file_storage.views import HomeView, upload_file, test_minio_connection
+from file_storage.views import HomeView, FileUploadView
 
 app_name = 'file_storage'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='list_files'),
-    path('upload/', upload_file, name='upload_file'),
+    path('upload/', FileUploadView.as_view(), name='upload_file'),
 ]
