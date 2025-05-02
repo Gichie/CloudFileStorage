9e3a1766165ffb08ccf68d6e0c2f86420fe35b1b
gichie <Gichie@mail.ru>
2025-05-02 23:25:27 +0300

    update

--- file_storage/models.py
+++ file_storage/models.py
@@ -2,19 +2,20 @@ import uuid
 
 from django.contrib.auth import get_user_model
 from django.db import models
-from storages.backends.s3boto3 import S3Boto3Storage
+
 
 User = get_user_model()
 
 
 def user_directory_path(instance, filename):
-    return f"user_{instance.user.id}/{filename}"
+    uuid_name = f"{uuid.uuid4()}_{filename}"
+    return f"user_{instance.user.id}/{uuid_name}"
 
 
 class UserFile(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='files')
-    file = models.FileField(upload_to=user_directory_path, storage=S3Boto3Storage())
+    file = models.FileField(upload_to=user_directory_path, null=True, blank=True)
     original_filename = models.CharField(max_length=255, blank=True)
     file_size = models.PositiveBigIntegerField(null=True, blank=True)
     content_type = models.CharField(max_length=100, null=True, blank=True)
@@ -25,10 +26,10 @@ class UserFile(models.Model):
         return f"{self.original_filename} (User: {self.user.username})"
 
     def save(self, *args, **kwargs):
-        if not self.pk and self.file:
-            self.original_filename = self.file.name.split('/')[-1]
+        if self.file and not self.original_filename:
+            self.original_filename = self.file.name
             self.file_size = self.file.size
-            self.content_type = self.file.content_type
+            self.content_type = self.file.file.content_type
 
         super().save(*args, **kwargs)
 
