970c50ef08942ae76f31323b48b162a5409a8f82
gichie <Gichie@mail.ru>
2025-05-12 16:11:58 +0300

    update urls

--- file_storage/urls.py
+++ file_storage/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 
-from file_storage.views import FileListView, FileUploadView, DirectoryCreateView, DeleteView, RenameView
+from file_storage.views import FileListView, DirectoryCreateView, DeleteView, RenameView, \
+    FileUploadAjaxView
 
 app_name = 'file_storage'
 
 urlpatterns = [
     path('', FileListView.as_view(), name='list_files'),
-    path('upload/', FileUploadView.as_view(), name='upload_file'),
-    path('create-folder/', DirectoryCreateView.as_view(), name='create_directory'),
+    path('create-folder/', DirectoryCreateView.as_view(), name='create_folder'),
 
-    path('delete/<uuid:pk>', DeleteView.as_view(), name='delete_item'),
-    path('rename/<uuid:pk>', RenameView.as_view(), name='rename_item'),
+    path('upload_ajax/', FileUploadAjaxView.as_view(), name='upload_file_ajax'),
+    path('delete/', DeleteView.as_view(), name='delete_item'),
+    path('rename/', RenameView.as_view(), name='rename'),
 ]
