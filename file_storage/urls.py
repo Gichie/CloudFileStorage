c67c8da5232e4259b586fcbc22ec0f5cb07ba69c
gichie <Gichie@mail.ru>
2025-05-15 16:20:32 +0300

    Remove the obsolete path create-folder/ in file_storage.urls

--- file_storage/urls.py
+++ file_storage/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 
-from file_storage.views import FileListView, DirectoryCreateView, DeleteView, RenameView, \
+from file_storage.views import FileListView, DeleteView, RenameView, \
     FileUploadAjaxView
 
 app_name = 'file_storage'
 
 urlpatterns = [
     path('', FileListView.as_view(), name='list_files'),
-    path('create-folder/', DirectoryCreateView.as_view(), name='create_folder'),
-
     path('upload_ajax/', FileUploadAjaxView.as_view(), name='upload_file_ajax'),
     path('delete/', DeleteView.as_view(), name='delete_item'),
     path('rename/', RenameView.as_view(), name='rename'),
