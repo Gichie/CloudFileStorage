62556d38ddc1756aab3d79ee97d50aaa4553a991
gichie <Gichie@mail.ru>
2025-05-21 23:31:03 +0300

    Add NameConflictError Exception

---
+++ file_storage/exceptions.py
@@ -0,0 +1,22 @@
+class NameConflictError(Exception):
+    """
+    Исключение, возникающее при конфликте имен файлов или папок.
+    """
+
+    def __init__(self, message, name, parent_name=None):
+        """
+        :param message: Общее сообщение об ошибке (для логов).
+        :param name: Имя объекта, вызвавшего конфликт.
+        :param parent_name: Имя родительской директории (для более понятного сообщения).
+        """
+        super().__init__(message)
+        self.name = name
+        self.parent_name = parent_name
+
+    def get_message(self):
+        """
+        Формирует сообщение для пользователя на основе деталей конфликта.
+        """
+        parent_message = f"в папке '{self.parent_name}'" if self.parent_name else "в текущей диретории"
+
+        return f"Файл или папка с таким именем({self.name}) уже существует {parent_message}"
