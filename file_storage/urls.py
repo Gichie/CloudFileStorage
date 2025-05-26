262f21809c68b61b4a9dc96ca72929e61f78d71b
gichie <Gichie@mail.ru>
2025-05-26 18:01:07 +0300

    Add paths for DownloadFileView and DownloadDirectoryView in urls

--- file_storage/urls.py
+++ file_storage/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 
 from file_storage.views import FileListView, DeleteView, RenameView, \
-    FileUploadAjaxView, DownloadFileView
+    FileUploadAjaxView, DownloadFileView, DownloadDirectoryView
 
 app_name = 'file_storage'
 
 urlpatterns = [
     path('', FileListView.as_view(), name='list_files'),
     path('upload_ajax/', FileUploadAjaxView.as_view(), name='upload_file_ajax'),
+    path('download/file/<uuid:file_id>', DownloadFileView.as_view(), name='download_file'),
+    path('download/directory/<uuid:directory_id>', DownloadDirectoryView.as_view(), name='download_directory'),
     path('delete/', DeleteView.as_view(), name='delete_item'),
     path('rename/', RenameView.as_view(), name='rename'),
-    path('download/file/<uuid:file_id>', DownloadFileView.as_view(), name='download_file')
+
 ]
